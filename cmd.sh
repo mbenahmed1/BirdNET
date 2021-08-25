a28d0dc8951df079f520b8e4ff4da16a75634a1e
mbenahmed1 <martin.ben-ahmed@web.de>
2021-08-25 21:28:40 +0200

    added params for evaluation run

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,9 @@
-python3 analyze.py --i bergmann/ --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.95 --overlap 1.5
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 0.0
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 1.0
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.0
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.5
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.9
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.95
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.99
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.995
+python3 analyze.py --i Deployment_2020_03_22 --csv-header --lat 52.3 --lon 8.0 --sensitivity 0.7 --min_conf 0.05 --overlap 2.999
